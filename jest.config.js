517478f6ebf519f7a3d53494112fbdfd92498f03
TauDuque <hectauduque@gmail.com>
2025-11-12 00:43:01 +0100

    chore(backend): configure Jest for unit testing
    - Setup ts-jest preset
    - Configure test environment for Node.js
    - Set coverage thresholds and reporting

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/tests'],
+  testMatch: ['**/*.test.ts'],
+  collectCoverageFrom: ['src/**/*.ts', '!src/index.ts', '!src/types/**'],
+  coverageDirectory: 'coverage',
+  coverageReporters: ['text', 'lcov', 'html'],
+  verbose: true,
+};
